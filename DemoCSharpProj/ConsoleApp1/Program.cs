f1c8e76645bbab5ea61b44a95e8fafc7f32774c6
Sebastien Siva <ssiva@ggc.edu>
2024-08-14 12:29:03 +0000

    Lecture 8_13

--- DemoCSharpProj/ConsoleApp1/Program.cs
+++ DemoCSharpProj/ConsoleApp1/Program.cs
@@ -4,8 +4,30 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-        Fruit f = new Fruit("Pear", 1.05);
-        Console.WriteLine(f);
+        for(int i = 0; i < 10; i++) {
+            Console.WriteLine("Hello World!");
+        }
+
+        float f = 0.5f;
+
+        Console.WriteLine("Hello World!" + f);
+
+        Random rgen = new Random();
+        for(int i = 0; i < 100; i++) {
+            int r = rgen.Next(1, 10);
+            Console.WriteLine(r);
+        }
+
+        int x = rgen.Next(1, 3);
+        if(x == 1) {
+            Console.WriteLine("Heads");
+        }
+        else {
+            Console.WriteLine("Tails");
+        }
+
+
+        //Fruit f = new Fruit("Pear", 1.05);
+        //Console.WriteLine(f);
     }
 }
